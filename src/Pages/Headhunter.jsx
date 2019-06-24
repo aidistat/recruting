ca7c283bba3d102d4f594990d807333bcceb53e8
adilet-reactjs <z.skinny.fy@gmail.com>
2019-06-24 11:31:37 +0600

    fixing bugs

--- src/Pages/Headhunter.jsx
+++ src/Pages/Headhunter.jsx
@@ -1,21 +1,21 @@
-import React, { Component } from "react";
-import Table from "../Components/Table/Table";
-import Filters from "../Components/Filters/Filters";
-import * as Constants from "../Constants/constants";
+import React, { Component } from 'react';
+import Table from '../Components/Table/Table';
+import Filters from '../Components/Filters/Filters';
+import * as Constants from '../Constants/constants';
 
 class HeadHunter extends Component {
-    state = {
-        sortedPeople: []
-    }
+  state = {
+    sortedPeople: []
+  };
 
-    render() {
-        return (
-            <div>
-                <Filters />
-                <Table headers={Constants.HEADERS} data={Constants.PEOPLE}/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Filters />
+        <Table headers={Constants.HEADERS} data={Constants.PEOPLE} />
+      </div>
+    );
+  }
 }
 
 export default HeadHunter;

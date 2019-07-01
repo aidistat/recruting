4c38b92d85865b3df083eac2fe2f9c7cd7d12834
iteremasov <teremasovjr@gmail.com>
2019-07-01 23:01:57 +0600

    restored <BrowserRouter> in index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./redux/redux-store";
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
+  <BrowserRouter>
   <Provider store={store}>
     <App />
-  </Provider>,
+  </Provider>
+  </BrowserRouter>,
   document.getElementById("root")
 );

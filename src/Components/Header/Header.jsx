12190d9ac3683491b446c1b8c546af7ff51b5fa5
adilet-reactjs <z.skinny.fy@gmail.com>
2019-07-05 17:17:57 +0600

    fixes

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import './header.css';
 import * as Constants from '../../Constants/constants';
+import {NavLink} from "react-router-dom";
 
 class Header extends Component {
-  render() {
+   render() {
     return (
       <div className="header">
         <div className="logo-icon">
@@ -16,7 +17,7 @@ class Header extends Component {
           {Constants.ROUTES.map((route, i) => {
             return (
               <li key={i}>
-                <a href={route.path}>{route.title}</a>
+                <NavLink to={route.path}> {route.title} </NavLink>
               </li>
             );
           })}

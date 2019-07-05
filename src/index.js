7767c8e1f75f3e42b02ade0d4c8bfb43f5eb5858
iteremasov <teremasovjr@gmail.com>
2019-07-05 15:48:41 +0600

    Fix index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css";
-import { Provider } from "react-redux";
-import store from "./redux/redux-store";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import { Provider } from 'react-redux';
+import store from './redux/redux-store';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter>
-  <Provider store={store}>
-    <App />
-  </Provider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
-  document.getElementById("root")
+  document.getElementById('root')
 );

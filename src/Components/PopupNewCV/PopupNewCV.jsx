12238efce8eb723060df611361cd602c90c7a8da
adilet-reactjs <z.skinny.fy@gmail.com>
2019-07-16 15:43:48 +0600

    fixes

--- src/Components/PopupNewCV/PopupNewCV.jsx
+++ src/Components/PopupNewCV/PopupNewCV.jsx
@@ -72,7 +72,7 @@ class PopupNewCV extends Component {
       adviser: From || '',
       url: URL,
       fullName: Name,
-      date: moment(new Date()).format('YYYY-MM-DDTHH:MM:SS'),
+      date: moment(new Date()).format('YYYY-MM-DDT00:00:00'),
       source: source,
       statuses: '',
       subject: '',
